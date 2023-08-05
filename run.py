803fcc1a28419269b8e4a39aafca72e8bb9f24d9
todiane <>
2023-08-05 21:19:40 +0000

    Add from and to dates before calculating pay

--- run.py
+++ run.py
@@ -2,21 +2,23 @@
 Main code for Corri Construction Company that appears in terminal
 """
 print("This is the Corri Construction Company Contractors Page.\n")
+print("Use this portal to input your hours for August and September 2023 only.")
+print("If your hours are for previous months please contact HR on 01305 483048\n")
 print("Please input your first and last name to begin\n")
 
 """
-instructions to add first and last name\n
+instructions to add first and last name
 """
-first_name = input("Enter your First Name:\n")
-last_name = input("Enter your Last Name:\n")
-print("Hello: " + first_name + " " + last_name)
+first_name = input("Enter your First Name: ")
+last_name = input("Enter your Last Name: ")
+print("Hello " + first_name + " " + last_name + ",\n")
 
 """
 instructions to confirm their profession by
 selecting one of the letters provided.
 
 """
-print("What is your job role " + first_name + "?")
+# print("What is your job role ?")
 
 
 def get_profession_choice():
@@ -37,14 +39,14 @@ def get_profession_choice():
         """
         Display available professions to the user
         """
-        print("Choose a profession:\n")
+        print("Select your profession:")
         for key, profession in professions.items():
             print(f"{key}: {profession['name']}")
 
         """
         Prompt the user to enter the letter corresponding to their choice
         """
-        choice = input("Enter your profession. Choose one of the above options:\n").lower()
+        choice = input("Select one of the above options a-f: ").lower()
 
         """
         Validate the users input and confirm their choice
@@ -56,7 +58,7 @@ def get_profession_choice():
             if confirm == "y":
                 return professions[choice]
         else:
-            print("Invalid choice. Please choose a valid option.")
+            print("Invalid choice. Please choose a valid option.\n")
 
 """
 if yes print out the details including random user number and hourly pay
@@ -65,11 +67,62 @@ if yes print out the details including random user number and hourly pay
 if __name__ == "__main__":
     chosen_profession = get_profession_choice()
     import random
+    print("Thank you.\n")
     print(first_name + " " + "your contractor number is " + str(random.randint(23203, 63944)))
     print(f"Your profession is: {chosen_profession['name']}")
     print(f"You earn £{chosen_profession['rate']} per hour.")
-    print("You pay 20% tax and 13% National Insurance")
+    print("You pay 20% tax and 13% National Insurance\n")
 
-hrs = input("Enter your hours:\n ")
+"""
+Get dates from the user for August or September 2023 only
+"""
+
+from datetime import datetime
+
+def get_date_from_user(prompt):
+    while True:
+        try:
+            date_str = input(prompt)
+            date_obj = datetime.strptime(date_str, "%d-%m-%Y")
+             # Check if the date is within the allowed range
+            if date_obj < datetime(2023, 8, 1) or date_obj > datetime(2023, 9, 30):
+                print("Dates should be between 1st August 2023 and 30th September 2023.")
+                print("If your hours are for previous months please contact HR on 01305 483048\n")
+            else:
+                return date_obj
+        except ValueError:
+            print("Invalid date format. Please enter a date in the format DD-MM-YYYY.")
+           
+
+if __name__ == "__main__":
+    print("Enter your 'from' and 'to' dates in the format DD-MM-YYYY for August and/or September 2023.")
+
+    from_date = get_date_from_user("Enter the 'from' date: ")
+    to_date = get_date_from_user("Enter the 'to' date: ")
+
+
+"""
+Asks user to input their hours and works out pay
+"""
+hrs = input("Enter your hours: ")
+print("Here are your potential pay details. If everything is authorised by your manager,\n")
 pay = float(hrs) * chosen_profession['rate'] 
-print("Your Weekly pay before tax is £" + str(pay))
+print(f"From: {from_date.strftime('%d-%m-%Y')}")
+print(f"To: {to_date.strftime('%d-%m-%Y')}")
+print("Your pay before tax is £" + str(pay) + " " + "for" + " " + str(hrs) + " " + "hours\n")
+
+# print("You will pay " +  + "in tax and" + + "National Insurance\n")
+
+
+# Print("Total potential pay after tax and NI is \n" )
+"""
+NEED TO ADD : IF EVERYTHING IS OK ASK IF THEY WANT TO EXIT
+IF THEY SAY NO EVERYTHING ISN'T OK - PRINT CONTACT HR ON 01304 849204
+
+THEN
+ASK IF THEY WANT TO ADD ANY MORE DATES
+print()
+
+DO YOU WANT TO ADD DATES - WORK FROM AND TO?
+
+"""
