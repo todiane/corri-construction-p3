9469f682cae193565839a1f382756aa069f0907c
todiane <>
2023-08-12 07:56:26 +0000

    Add type_print to type out letters

--- run.py
+++ run.py
@@ -2,35 +2,53 @@
 
 # imports ----------
 import datetime
+import time,os,sys
 from flask import Flask
-import colorama
 from colorama import Fore, Back, Style
-colorama.init(autoreset=True)
 # --------------
 
+# Typing effect for letters
+
+def type_print(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    print('')
+
+# Clears the screen at the very end
+
+def clearScreen():
+  os.system("clear")
+
+
 # Corri Construction Company Introduction
+
+
 text = Fore.WHITE + '\033[1m' + "CORRI CONSTRUCTION COMPANY CONTRACTORS PAGE" + '\033[0m'
 new_text = text.center(90)
 print(new_text)
 
 # Instructions for contractors
-print(Fore.YELLOW + '\033[1m' + "\nUse this portal to input your August(08) and September(09) 2023 hours.\n")
-print("If your hours are for previous months please contact HR on 01305 483048")
-print("\nINSTRUCTIONS: (please read carefully)")
+type_print(Fore.YELLOW + '\033[1m' + "\nUse this portal to input your August(08) and September(09) 2023 hours.\n")
+type_print(Fore.WHITE + "If your hours are for previous months please contact HR on 01305 483048")
+time.sleep(1)
+type_print("\nINSTRUCTIONS: (please read carefully)")
+time.sleep(1)
 print(Fore.RED + "\nInformation once entered CANNOT be amended." + Fore.WHITE + "If you make an error")
 print("and want to resubmit, press the " + Fore.GREEN + "'RUN CONTRACTOR PROGRAM'" + Fore.WHITE + " button.\n")
-
-print("Failure to input your NAME correctly may result in your pay being delayed.")
+time.sleep(2)
+type_print("Failure to input your NAME correctly may result in your pay being delayed.\n")
 print("Our system uses your " + Fore.GREEN + "name and employee number" + Fore.WHITE + " to find you so it is important")
 print("that you enter the FULL NAME you provided for our records.")
 print("e.g. " + Fore.GREEN + "Mark Jenkins" + Fore.WHITE + ", although " + Fore.GREEN + "mark jenkins " + Fore.WHITE + "and " + Fore.GREEN + "MARK JENKINS " + Fore.WHITE + "are also ok.\n")
-
-print("All Clear? Great. Let's get started - otherwise contact HR\n")
+time.sleep(3)
+type_print("All Clear? Great. Let's get started - otherwise contact HR\n")
 print(Fore.GREEN + "Input your FULL (First + Last) name to begin:\n")
 
 # Instructions to add first and last name. Used strip() to detect if section left blank
 while True:
-    first_name = input("Enter your First Name: ")
+    first_name = input(Fore.WHITE + "Enter your First Name: ")
     if first_name.strip() == "":    # code found at GeekforGeek
         print("First name is required. Please enter a valid first name.")
         continue
@@ -40,7 +58,7 @@ while True:
         continue
     break
 
-print(Fore.YELLOW + "H E L L O, " + first_name + " " + last_name + "\n")
+type_print(Fore.YELLOW + "H E L L O, " + first_name + " " + last_name + "\n")
 
 # Instructions to confirm their profession by selecting a letter.
 
@@ -61,7 +79,9 @@ def get_profession_choice():
 
     while True:
         # Display available professions to the user
-        print(Fore.GREEN + "Select your profession:")
+        type_print(Fore.WHITE + "Select your profession:")
+        time.sleep(1)
+        print('')
         for key, profession in professions.items():
             print(f"{key}: {profession['name']}")
 
@@ -86,17 +106,21 @@ def get_profession_choice():
 if __name__ == "__main__":
     chosen_profession = get_profession_choice()
     import random
-    print(Fore.WHITE + "Thank you.\n")
-    print(Fore.GREEN + first_name + " your contractor number is " + str(random.randint(23203, 63944)))
-    print(f"Your profession is: {chosen_profession['name']}")
-    print(Fore.GREEN + f"You earn £{chosen_profession['rate']} per hour.")
-    print("You pay 20% tax and 13% National Insurance\n")
+    type_print(Fore.WHITE + "Thank you.\n")
+    time.sleep(1)
+    print(first_name + " your contractor number is " + str(random.randint(23203, 63944)))
+    type_print(f"Your profession is: {chosen_profession['name']}")
+    time.sleep(1)
+    type_print(Fore.GREEN + f"You earn £{chosen_profession['rate']} per hour.")
+    time.sleep(1)
+    type_print(Fore.WHITE + "You pay 20% tax and 13% National Insurance\n")
     # print("Earnings this year are £" + str(random.randint(67500, 80000)) + " so you will pay " + {chosen_profession['tax']} + "% tax and 13% National Insurance\n")
 
 # Ask user for dates and hours
-print(Fore.GREEN + "\nNext, we need to know the dates you worked (From - To) and number of hours.\n")
-print("This is for the months of August (08) and September(09) only.")
-
+type_print(Fore.GREEN + "\nNext, we need to know the dates you worked (From - To) and number of hours.\n")
+time.sleep(1)
+type_print(Fore.WHITE + "This is for the months of August (08) and September(09) only.")
+time.sleep(0.5)
 """
 Get dates from the user for August or September 2023 only. Checks if the dates
 are within that range and in sequential order if not error message given.
@@ -149,16 +173,17 @@ def get_from_to_dates():
 from_date, to_date = get_from_to_dates()
 
 
-print(Fore.GREEN + f"Thank you, you entered from {from_date.strftime('%d-%m-%Y')} to {to_date.strftime('%d-%m-%Y')} as your dates.\n")
-
+type_print(Fore.GREEN + f"Thank you, you entered from {from_date.strftime('%d-%m-%Y')} to {to_date.strftime('%d-%m-%Y')} as your dates.\n")
+time.sleep(1)
 
 # Asks user to input their hours and works out pay
-hrs = input("Enter your hours: ")
+hrs = input(Fore.WHITE + "Enter your hours: ")
 
 # Ask user to confirm the information added is correct
 info_confirm = input(Fore.GREEN + f"\nCheck that the information added so far is correct & confirm. Enter (y/n): ").lower()
 if info_confirm == "y":
-    print(Fore.WHITE + "\nThank you, " + first_name + ".")
+    type_print(Fore.WHITE + "\nThank you, " + first_name + ".")
+    time.sleep(1)
 
 else:
     print(Back.RED + Fore.WHITE + "\nOkay. You can't edit anything already entered " + first_name + ",")
@@ -166,10 +191,11 @@ else:
     quit()
 
 # Confirm information of profession, dates and hours plus gives before tax amount
-print("\nThis information will be authorised by your manager:\n")
+type_print("\nThis information will be authorised by your manager:\n")
+time.sleep(1)
 pay = float(hrs) * chosen_profession['rate']
-print((f"From {from_date.strftime('%d-%m')}") + " " + (f"to {to_date.strftime('%d-%m')}") + " 2023, your pay before tax is £" + str(pay) + " for" + " " + str(hrs) + " hours\n")
-
+type_print((f"From {from_date.strftime('%d-%m')}") + " " + (f"to {to_date.strftime('%d-%m')}") + " 2023, your pay before tax is £" + str(pay) + " for" + " " + str(hrs) + " hours\n")
+time.sleep(1)
 # Calculating payment after tax - 20% tax is 0.2 and 13% NI is 0.13
 
 
@@ -188,21 +214,42 @@ national_insurance_amount = national_insurance * pay
 # Print out full result of pay minus tax and NI
 pay_statement = Fore.YELLOW + '\033[1m' + (f" Your pay minus tax of (£{tax_amount:.2f}) and NI of (£{national_insurance_amount:.2f}) is £{pay_after:.2f}") + '\033[0m'
 pay_summary = pay_statement.center(90)
+type_print(pay_summary)
+time.sleep(1)
 
-print(pay_summary)
-print(Fore.RED + "\nThe TAX and NATIONAL INSURANCE amounts shown are for your information only\n")
-print("Final pay amounts are approximate and depend on your tax status.")
-print("The actual amount you are paid may change.\n")
-print("If you have any questions contact HR on 01305 483048.\n")
+type_print(Fore.WHITE + "\nThe TAX and NATIONAL INSURANCE amounts shown are for your information only")
+time.sleep(1)
+print('')
+type_print("Final pay amounts are approximate and depend on your tax status.")
+time.sleep(1)
+type_print("The actual amount you are paid may change.")
+time.sleep(1)  
+print('')
+type_print("If you have any questions contact HR on 01305 483048.\n")
 
 # Ask user if they want to exit
 exit = input(Fore.GREEN + f"Information Complete. Select y to submit or n to continue (y/n): ").lower()
 if exit == "y":
-    print(Fore.WHITE + "\nInformation successfully submitted to HR. Thank you " + first_name + ".")
-    print("A copy has been sent to the email address we have on file for you.")
+    type_print(Fore.WHITE + "\nInformation successfully submitted to HR. Thank you " + first_name + ".")
+    time.sleep(1)
+    print('')
+    type_print("A copy has been sent to the email address we have on file for you.")
     print("If you have any questions contact HR on 01305 483048.\n")
-    quit()
+    time.sleep(1)
+    # quit()
 
 else:
     print(Fore.WHITE + "\nOkay. You can't edit anything already entered " + first_name + ",")
     print("but you can hit the Run Program button above to start again.\n")
+
+# Clear screen message once everything is complete
+
+type_print("Your session is now complete.\n")
+time.sleep(2)
+type_print("This screen will clear itself in 3..")
+time.sleep(1)
+type_print("2..")
+time.sleep(1)
+type_print("1..")
+time.sleep(1)
+clearScreen()
