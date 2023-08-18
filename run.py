50c56ab55d1200575bc0a2a04440aec3fa399813
todiane <>
2023-08-18 16:22:35 +0000

    Tidy code by reducing lines that are too long

--- run.py
+++ run.py
@@ -19,27 +19,49 @@ def type_print(text):
         time.sleep(0.05)
     print('')
 
+
+# function to check that no more than 13 days are worked in one day
+
+
+def worked_too_many_hours(days_worked, hours_entered):
+    max_hours_per_day = 13
+    max_total_hours = days_worked * max_hours_per_day
+    if hours_entered > max_total_hours:
+        return True
+    return False
+
+
+
+
 # Corri Construction Company Introduction
 
 
-text = Fore.WHITE + '\033[1m' + "CORRI CONSTRUCTION COMPANY CONTRACTORS PAGE" + '\033[0m'
+text = Fore.WHITE + '\033[1m' + \
+    "CORRI CONSTRUCTION COMPANY CONTRACTORS PAGE" + '\033[0m'
 new_text = text.center(90)
 print(new_text)
 
 # Instructions for contractors
 
-type_print(Fore.YELLOW + '\033[1m' + "\n Use this portal to input your August(08) and September(09) 2023 hours.\n")
-type_print(Fore.WHITE + " If your hours are for previous months please contact HR on 01305 483048")
+type_print(Fore.YELLOW +
+           '\033[1m' + "\n Use this portal to input your August(08) and September(09) 2023 hours.\n")
+type_print(Fore.WHITE +
+           " If your hours are for previous months please contact HR on 01305 483048")
 time.sleep(1)
 type_print("\n INSTRUCTIONS: (please read carefully)")
 time.sleep(1)
-type_print(Fore.RED + "\n Information once entered CANNOT be amended. " + Fore.WHITE + "If you make an error")
-type_print(" and want to resubmit, press the " + Fore.GREEN + "'RUN CONTRACTOR PROGRAM'" + Fore.WHITE + " button.\n")
+type_print(Fore.RED + "\n Information once entered CANNOT be amended. " +
+           Fore.WHITE + "If you make an error")
+type_print(" and want to resubmit, press the " + Fore.GREEN +
+           "'RUN CONTRACTOR PROGRAM'" + Fore.WHITE + " button.\n")
 time.sleep(2)
-type_print(" Failure to input your NAME correctly may result in your pay being delayed.\n")
-type_print(" Our system uses your " + Fore.GREEN + "name and employee number" + Fore.WHITE + " to find you so it is important")
+type_print(
+    " Failure to input your NAME correctly may result in your pay being delayed.\n")
+type_print(" Our system uses your " + Fore.GREEN +
+           "name and employee number" + Fore.WHITE + " to find you so it is important")
 type_print(" that you enter the FULL NAME you provided for our records.")
-type_print(" e.g. " + Fore.GREEN + "Mark Jenkins" + Fore.WHITE + ", although " + Fore.GREEN + "mark jenkins " + Fore.WHITE + "and " + Fore.GREEN + "MARK JENKINS " + Fore.WHITE + "are also ok.\n")
+type_print(" e.g. " + Fore.GREEN + "Mark Jenkins" + Fore.WHITE + ", although " + Fore.GREEN +
+           "mark jenkins " + Fore.WHITE + "and " + Fore.GREEN + "MARK JENKINS " + Fore.WHITE + "are also ok.\n")
 time.sleep(3)
 type_print(" All Clear? Great. Let's get started - otherwise contact HR\n")
 time.sleep(0.5)
@@ -55,29 +77,37 @@ Used strip() to detect if section left blank
 while True:
     first_name = input(Fore.WHITE + " Enter your First Name: ")
     if first_name.strip() == "":     # code found at GeeksforGeeks
-        type_print(Fore.RED + " First name is required. Please enter a valid first name.")
+        type_print(
+            Fore.RED + " First name is required. "
+            "Please enter a valid first name.")
         continue
     elif not first_name.isalpha():   # code found at GeeksforGeeks
-        type_print(Fore.RED + " First name should not contain numbers or symbols. Enter a valid first name.")
+        type_print(
+            Fore.RED + " First name should not contain numbers  " 
+            "or symbols. Enter a valid first name.")
         continue
     last_name = input(" Enter your Last Name: ")
     if last_name.strip() == "":
-        type_print(Fore.RED + " Last name is required. Please enter a valid First & Last name.")
+        type_print(
+            Fore.RED + " Last name is required. Please enter "
+            "a valid First & Last name.")
         continue
     elif not last_name.isalpha():
-        type_print(Fore.RED + " Your name should not contain numbers or symbols. Re-enter name.")
+        type_print(
+            Fore.RED + " Your name should not contain numbers " 
+            " or symbols. Re-enter name.")
         continue
     break
 
 type_print(Fore.YELLOW + " HELLO, " + first_name + " " + last_name + "\n")
 
-# Instructions to confirm their profession by selecting a letter.
+# Profession confirmed by selecting a letter.
 
 
 def get_profession_choice():
     """
-    Dictionary to map profession codes to profession names
-    and rate of pay to each profession
+    Dictionary to map profession codes to profession
+    names and rate of pay to each profession
     """
     professions = {
         "a": {"name": "Bricklayer", "rate": 28.87, },
@@ -96,8 +126,15 @@ def get_profession_choice():
         for key, profession in professions.items():
             print(f"{key}: {profession['name']}")
 
-# Prompt the user to enter the letter corresponding to their choice
-        choice = input("\n Select one of the above options " + Fore.GREEN + "a-f: ").lower()
+        """
+
+        Prompt user to enter the letter corresponding 
+        to their choice
+
+        """
+
+        choice = input("\n Select one of the above options " +
+                       Fore.GREEN + "a-f: ").lower()
 
         """
         Validate the users input and confirm their choice
@@ -105,13 +142,20 @@ def get_profession_choice():
         list of options again
         """
         if choice in professions:
-            confirm = input(f" You chose {professions[choice]['name']}. Is this correct? Enter (y/n) only: ").lower()
+            confirm = input(
+                f" You chose {professions[choice]['name']}. "
+                "Is this correct? Enter (y/n) only: ").lower()
             if confirm == "y":
                 return professions[choice]
         else:
-            print(Fore.RED + " Error: Invalid choice. Please choose a valid option.\n")
+            print(Fore.RED + " Error: Invalid choice." 
+            " Please choose a valid option.\n")
 
-# If yes print out the details including random user number and hourly pay
+"""
+
+If yes print out the details including random user number and hourly pay
+
+"""
 
 
 if __name__ == "__main__":
@@ -119,19 +163,21 @@ if __name__ == "__main__":
     import random
     type_print(Fore.WHITE + " Thank you.\n")
     time.sleep(1)
-    print(" " + first_name + " your contractor number is " + str(random.randint(23203, 63944)))
+    print(" " + first_name + " your contractor number is " +
+          str(random.randint(23203, 63944)))
     type_print(f" Your profession is: {chosen_profession['name']}")
     time.sleep(1)
     type_print(Fore.GREEN + f" You earn £{chosen_profession['rate']} per hour")
     time.sleep(1)
     type_print(Fore.WHITE + " You pay 20% tax and 13% National Insurance\n")
 
-# Ask user for dates and hours
-type_print(Fore.GREEN + "\n Next, we need to know the dates you worked (From - To)")
-type_print(Fore.GREEN + " plus the number of days and number of hours worked.\n")
-time.sleep(0.5)
-type_print(Fore.WHITE + " This is for the months of August (08) and September(09) only.\n")
-time.sleep(0.5)
+    # Ask user for dates and hours
+    type_print(Fore.GREEN + "\n Next, we need to know the dates you worked (From - To)")
+    type_print(Fore.GREEN + " plus the number of days and number of hours worked.\n")
+    time.sleep(0.5)
+    type_print(Fore.WHITE +
+           " This is for the months of August (08) and September(09) only.\n")
+    time.sleep(0.5)
 
 """
 Asks users to input their days worked, number of days and total number of hours
@@ -140,21 +186,16 @@ the right order, and the number of days added fits in with the dates inputted.
 
 Code added with help from Travis.Media
 """
-# function to check that no more than 13 days are worked in one day
 
-def worked_too_many_hours(days_worked, hours_entered):
-    max_hours_per_day = 13
-    max_total_hours = days_worked * max_hours_per_day
-    if hours_entered > max_total_hours:
-        return True
-    return False
-
+# User enters date, days and hours worked
 
 while True:
     try:
-        start_date_str = input(Fore.WHITE + " Enter the start date (DD-MM-YYYY): ")
+        start_date_str = input(
+            Fore.WHITE + " Enter the start date (DD-MM-YYYY): ")
         end_date_str = input(Fore.WHITE + " Enter the end date (DD-MM-YYYY): ")
-        days_worked = int(input(Fore.WHITE + " Enter the number of days worked: "))
+        days_worked = int(
+            input(Fore.WHITE + " Enter the number of days worked: "))
         hrs = float(input(Fore.WHITE + " Enter your hours: "))
 
         start_date = datetime.strptime(start_date_str, "%d-%m-%Y")
@@ -171,29 +212,36 @@ while True:
         calculated_days_worked = (end_date - start_date).days + 1
 
         if days_worked != calculated_days_worked:
-            print(Fore.RED + " Error: The dates entered and the number of days worked do not match.")
+            print(
+                Fore.RED + " Error: The dates entered and the number of days worked do not match.")
             continue
 
         if worked_too_many_hours(days_worked, hrs):
-            print(Fore.RED + " Error: You cannot enter more than 13 hours per day for a total of " + str(days_worked) + " days.")
+            print(Fore.RED + " Error: You cannot enter more than 13 hours per day for a total of " +
+                  str(days_worked) + " days.")
         else:
-            print(" You have entered " + str(hrs) + " hours for " + str(days_worked) + " days.")
+            print(" You have entered " + str(hrs) +
+                  " hours for " + str(days_worked) + " days.")
             break
     except ValueError:
-        print(Fore.RED + " Error: Check your dates match, your days add up and/or your hours.")
+        print(
+            Fore.RED + " Error: Check your dates match, your days add up and/or your hours.")
         print(Fore.RED + " NUMBERS ONLY in this section. Anything else is invalid and you will have to re-enter.")
 
 # Ask user to confirm the information added is correct
-info_confirm = input(Fore.GREEN + f"\n Confirm the information added so far is correct. Enter (y/n) only: ").lower()
+info_confirm = input(
+    Fore.GREEN + f"\n Confirm the information added so far is correct. Enter (y/n) only: ").lower()
 
 while info_confirm not in ["y", "n"]:
     print(Fore.RED + "\n Error: Invalid input. Please enter y or n only.")
-    info_confirm = input(Fore.GREEN + "\n Please enter either y or n to continue: ").lower()
+    info_confirm = input(
+        Fore.GREEN + "\n Please enter either y or n to continue: ").lower()
 
 if info_confirm == "y":
     print(Fore.WHITE + "\n Thank you, " + first_name + ".")
 else:
-    print(Back.RED + Fore.WHITE + "\n Okay. You can't edit anything already entered " + first_name + ",") 
+    print(Back.RED + Fore.WHITE +
+          "\n Okay. You can't edit anything already entered " + first_name + ",")
     print(Fore.WHITE + " but you can restart the program to start again.")
     sys.exit()
 
@@ -202,7 +250,8 @@ else:
 type_print("\n This information will be authorised by your manager:\n")
 time.sleep(1)
 pay = float(hrs) * chosen_profession['rate']
-type_print((f" From {start_date.strftime('%d-%m')}") + " " + (f"to {end_date.strftime('%d-%m')}") + f" 2023, your pay before tax is £{pay:.2f} for {hrs} hours")
+type_print((f" From {start_date.strftime('%d-%m')}") + " " + (f"to {end_date.strftime('%d-%m')}") +
+           f" 2023, your pay before tax is £{pay:.2f} for {hrs} hours")
 time.sleep(1)
 
 # Calculates amount after tax and national insurance deductions
@@ -212,7 +261,7 @@ def final_pay(pay, tax, national_insurance):
     pay_after = pay - (tax * pay) - (national_insurance * pay)
     return pay_after
 
-
+# tax and national insurance calculations
 tax = 0.02
 national_insurance = 0.013
 pay_after = final_pay(pay, tax, national_insurance)
@@ -221,12 +270,14 @@ national_insurance_amount = national_insurance * pay
 
 
 # Print out full result of pay minus tax and NI
-pay_statement = Fore.YELLOW + (f"\n Your pay minus tax of (£{tax_amount:.2f}) and NI of (£{national_insurance_amount:.2f}) is £{pay_after:.2f}")
+pay_statement = Fore.YELLOW + \
+    (f"\n Your pay minus tax of (£{tax_amount:.2f}) and NI of (£{national_insurance_amount:.2f}) is £{pay_after:.2f}")
 pay_summary = pay_statement.center(80)
 type_print(pay_summary)
 time.sleep(1)
 
-type_print(Fore.WHITE + "\n The TAX and NATIONAL INSURANCE amounts shown are for your information only")
+type_print(Fore.WHITE +
+           "\n The TAX and NATIONAL INSURANCE amounts shown are for your information only")
 time.sleep(1)
 print('')
 type_print(" Final pay amounts are approximate and depend on your tax status.")
@@ -236,16 +287,19 @@ time.sleep(1)
 print('')
 type_print(" If you have any questions contact HR on 01305 483048.\n")
 
-# Ask user if they want to exit  
+# Ask user if they want to exit
 while True:
-    exit = input(Fore.GREEN + " Information Complete. Select y to submit or n to continue (y/n): ").lower()
-    
+    exit = input(
+        Fore.GREEN + " Information Complete. Select y to submit or n to continue (y/n): ").lower()
+
     if exit == "y":
-        type_print(Fore.WHITE + "\n Information successfully submitted to HR. Thank you " + first_name + ".")
+        type_print(
+            Fore.WHITE + "\n Information successfully submitted to HR. Thank you " + first_name + ".")
         type_print(Fore.WHITE + "\n You can now exit the program.")
         sys.exit()
     elif exit == "n":
-        type_print(Back.RED + Fore.WHITE + "\n Okay. You can't edit anything already entered " + first_name + ",")
+        type_print(Back.RED + Fore.WHITE +
+                   "\n Okay. You can't edit anything already entered " + first_name + ",")
         type_print(" but you can restart the program to start again.")
         sys.exit()
     else:
